defc1aab92344b4eb4bcb3b2bc2869433cceaf53
cesarbouzas <cesarbouzaspaw@gmail.com>
2022-09-29 16:24:34 +0200

    p1

---
+++ MP0488_Desenvolvemento de interfaces/001_unidad_1/009_Java/AplicacionEscritorio/src/valores/AplicacionEscritorio.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package valores;
+
+/**
+ *
+ * @author cesar
+ */
+public class AplicacionEscritorio {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
