0a26d6aac79954652b1f8246400cb1291d90bd64
cesarbouzas <cesarbouzaspaw@gmail.com>
2022-12-10 10:50:17 +0100

    examen

---
+++ MP0488_Desenvolvemento de interfaces/repasoExamenHotel/src/repasoexamenhotel/FXMLDocumentController.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXML2.java to edit this template
+ */
+package repasoexamenhotel;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+
+/**
+ *
+ * @author Usuario
+ */
+public class FXMLDocumentController implements Initializable {
+    
+    @FXML
+    private Label label;
+     @FXML
+    private ComboBox cb;
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+        System.out.println("You clicked me!");
+        label.setText("Hello World!");
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        cb.getItems().addAll("simple","doble","triple","tu puta madre");
+    }    
+    
+}
